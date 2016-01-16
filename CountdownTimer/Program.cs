dfdc5ef6e042d706efc500b832cdc31ba331fb06
Yvaine8 <maayan.moo@gmail.com>
2016-01-16 19:25:41 +0200

    First commit

---
+++ CountdownTimer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CountdownTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime countdownEnd;
+            TimeSpan countdownTime;
+            Console.WriteLine("Enter countdown time in HH:MM:SS format:");
+
+            string[] input = Console.ReadLine().Split(new char[] { ':' });
+            while (input.Length == 3)
+            {
+                countdownTime = new TimeSpan(int.Parse(input[0]), int.Parse(input[1]), int.Parse(input[2]) + 1);
+                countdownEnd = DateTime.Now.Add(countdownTime);
+
+                Console.Clear();
+                Console.WriteLine("STARTING COUNTDOWN!");
+                Thread.Sleep(500);
+                Console.Clear();
+
+                while (DateTime.Now < countdownEnd)
+                {
+                    countdownTime = countdownEnd - DateTime.Now;
+                    Console.WriteLine(countdownTime.ToString(@"hh\:mm\:ss"));
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                }
+
+                Console.Clear();
+                Console.Beep();
+                Console.WriteLine("***COUNTDOWN FINISHED!!!***\n\nEnter a new countdown time or 'quit'");
+                input = Console.ReadLine().Split(new char[] { ':' });
+            }
+        }
+    }
+}
